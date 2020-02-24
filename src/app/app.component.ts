ec550117b0b12aa7c7defa433e86774740672870
Cesar Espitia <shiroukamui001@gmail.com>
2020-02-24 17:52:22 -0500

    Instalando librerías con NPM (Google Maps)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import { environment } from 'src/environments/environment'
+import * as mapboxgl from 'mapbox-gl'
 
 @Component({
   selector: 'app-root',
@@ -6,8 +8,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.sass']
 })
 
-export class AppComponent {
+export class AppComponent implements OnInit{
 
+  map: mapboxgl.Map
+  coordinates: any
+  lng: number = -74.0700526
+  lat: number = 4.6489681
   places: any = [
     { active: true, name: 'Floreria la Gardenia' },
     { active: true, name: 'Donas la Delicia' },
@@ -16,4 +22,27 @@ export class AppComponent {
     { active: true, name: 'Hotel el Descanso' },
     { active: false, name: 'Zapateria el Clavo' }
   ]
+
+  ngOnInit(): void {
+    mapboxgl.accessToken = environment.mapboxKey
+    this.map = new mapboxgl.Map({
+      container: 'map',
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [this.lng, this.lat],
+      zoom: 10
+    })
+    this.createMarker(this.lng, this.lat)
+  }
+
+  createMarker(lng: number, lat: number): void {
+    const marker = new mapboxgl.Marker({
+      draggable: true
+    })
+    .setLngLat([lng, lat])
+    .addTo(this.map)
+
+    marker.on('drag', () => {
+      this.coordinates = marker.getLngLat()
+    })
+  }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  mapboxKey: 'pk.eyJ1Ijoia2FtdWkwMDEiLCJhIjoiY2s2cDhubHNjMWpxcDNlbnl6bnh3YmJ3OCJ9.S0vq-SqTMdinLtLnqludTQ'
 };
 
 /*
